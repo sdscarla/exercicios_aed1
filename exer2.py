0799558e0dd08f66fee08456b35921466b4b3a96
linlin <sdsantoscarla@gmail.com>
2025-05-24 21:45:40 -0300

    exercicio 2

---
+++ exer2.py
@@ -0,0 +1,8 @@
+#Ler um número inteiro. Apresentar o seu antecessor e o seu sucessor
+
+n = int(input("Insira um número: "))
+
+antecessor = n - 1
+sucessor = n + 1
+
+print(f"O antecessor de {n} é {antecessor} e o sucessor de {n} é {sucessor}")
